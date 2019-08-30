e6421fd51b9477bfa88b2d3c6244bf25444c7ffc
junya.nishimura <junya.nishimura.gp@gmail.com>
2019-08-30 20:21:22 +0900

    04.js remove

--- js/04.js
+++
@@ -1,9 +0,0 @@
-/* ------------------------------------
-sec076 typeof
--------------------------------------*/
-
-function sec076() {
-	const inputTxt = document.getElementById("input076").value;
-	const result = typeof inputTxt;
-	alert(result);
-}
