2a97251f43c4bdc315b11c1030310f3a9a39fb97
junya.nishimura <junya.nishimura.gp@gmail.com>
2019-08-30 20:20:34 +0900

    chapter4

---
+++ js/04.js
@@ -0,0 +1,9 @@
+/* ------------------------------------
+sec076 typeof
+-------------------------------------*/
+
+function sec076() {
+	const inputTxt = document.getElementById("input076").value;
+	const result = typeof inputTxt;
+	alert(result);
+}
