1ed9bb23d0d7d79e8226b3ccc3d48145e7dac208
junya.nishimura <junya.nishimura.gp@gmail.com>
2019-10-07 19:06:58 +0900

    175

--- js/09.js
+++ js/09.js
@@ -119,7 +119,7 @@ sec168 ローカルファイルをテキストとして読み込む
 
 function sec168() {
 	const input168 = document.querySelector('#input168');
-	const log  = document.querySelector('.log')
+	const log = document.querySelector('.log168');
 	input168.addEventListener('input', (e) => {
 		const files = e.target.files;
 		const file = files[0];
@@ -127,9 +127,94 @@ function sec168() {
 		const reader = new FileReader();
 		reader.addEventListener('load', () => {
 			//load後resultを.logに出力
-			log.textContent = 'aa';
+			log.textContent = reader.result;
 		});
 		//テキストファイルとして読み込む
 		reader.readAsText(file);
 	});
 } sec168();
+
+/* ------------------------------------
+sec169 ローカルファイルをDataURLとして読み込む
+-------------------------------------*/
+
+function sec169() {
+	const input169 = document.querySelector('#input169');
+	const log = document.querySelector('.log169 img');
+	input169.addEventListener('input', (e) => {
+		const files = e.target.files;
+		const file = files[0];
+		//FileReaderのインスタンス作成
+		const reader = new FileReader();
+		reader.addEventListener('load', () => {
+			//load後resultを.logに出力
+			log.src = reader.result;
+		});
+		//テキストファイルとして読み込む
+		reader.readAsDataURL(file);
+	});
+} sec169();
+
+/* ------------------------------------
+sec171 ラジオボタンの変更を検知
+-------------------------------------*/
+
+function sec171() {
+	const form171 = document.querySelector('#form171');
+	const color171 = document.querySelector('.color171 span');
+	const food171 = document.querySelector('.food171 span');
+	form171.addEventListener('change', handleChange);
+
+	function handleChange(e) {
+		const colorValue = form171.color.value;
+		const foodValue = form171.food.value;
+		color171.innerHTML = colorValue;
+		food171.innerHTML = foodValue;
+	}
+} sec171();
+
+/* ------------------------------------
+sec173 ドロップダウンメニューの変更を検知
+-------------------------------------*/
+
+function sec173() {
+	const select173 = document.querySelector('#select173');
+	const result173 = document.querySelector('.result173 span');
+	select173.addEventListener('change', handleChange);
+
+	function handleChange(e) {
+		const selectValue = select173.value;
+		result173.innerHTML = selectValue;
+	}
+} sec173();
+
+/* ------------------------------------
+sec174 スライダーの情報を取得
+-------------------------------------*/
+
+function sec174() {
+	const range174 = document.querySelector('#range174');
+	const result174 = document.querySelector('.result174');
+	const value174 = range174.value;
+	result174.innerHTML = value174;
+}
+
+/* ------------------------------------
+sec175 スライダーの情報を取得
+-------------------------------------*/
+
+function sec175() {
+	const range175 = document.querySelector('#range175');
+	const result175i = document.querySelector('.result175i');
+	const result175c = document.querySelector('.result175c');
+	range175.addEventListener('input', handleInput);
+	range175.addEventListener('change', handleChange);
+	function handleInput(e) {
+		const value175 = e.target.value;
+		result175i.innerHTML = value175;
+	}
+	function handleChange(e) {
+		const value175 = e.target.value;
+		result175c.innerHTML = value175;
+	}
+} sec175();
